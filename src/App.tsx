a961e9ff92f206009af895ee5788f524d212626b
WM-IL000542 <wm-il000542@WM-IL000542ui-MacBookPro.local>
2023-03-03 17:32:07 +0900

    03031732

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { EditorState, convertToRaw } from "draft-js";
+import { EditorState, convertToRaw, AtomicBlockUtils } from "draft-js";
 import draftToHtml from "draftjs-to-html";
 import ReactHtmlParser from 'react-html-parser';
 
@@ -18,11 +18,12 @@ function App(this: any) {
     convertToRaw(editorState.getCurrentContent())
   );
 
-  const uploadImageCallBack = (file: any) => {
+  const uploadImageCallBack = (file: File) => {
     console.log("aaaa");
+    
     return new Promise((resolve, reject) => {
-      console.log("bbbb");
-      resolve({ data: { link: "https://search.pstatic.net/sunny/?src=https%3A%2F%2Fthumb2.gettyimageskorea.com%2Fimage_preview%2F700%2F202301%2FFPL%2F1455340739.jpg&type=sc960_832" } });
+      console.log("bbbb:", URL.createObjectURL(file));
+      resolve({ data: { link: URL.createObjectURL(file) } });
     });
   };
 
@@ -35,33 +36,50 @@ function App(this: any) {
     }
   };
 
+  const handleEditorDrop = async (e: any) => {
+    e.preventDefault();
+    if (e.dataTransfer.files.length > 0) {
+      const file = e.dataTransfer.files[0];
+      if (file.type.startsWith('image')) {
+        const newFile: any = await uploadImageCallBack(file);
+        const entityData = { src: newFile.data.link, height: newFile.data?.metadata?.height || '320px', width: newFile.data?.metadata?.width || '320px' };
+        const entityKey = editorState.getCurrentContent().createEntity('IMAGE', 'MUTABLE', entityData).getLastCreatedEntityKey();
+        const newEditorState = AtomicBlockUtils.insertAtomicBlock(editorState, entityKey, ' ');
+        setEditorState(newEditorState);
+      }
+  } 
+  };
+
   return (
     <div className="App">
       <div className="notice__wrap">
           <h2 className="notice__title">내용</h2>
-          <Editor
-            // 툴바 설정
-            toolbar={{
-              // inDropdown: 해당 항목과 관련된 항목을 드롭다운으로 나타낼것인지
-              list: { inDropdown: true },
-              textAlign: { inDropdown: true },
-              link: { inDropdown: true },
-              history: { inDropdown: false },
-              image: {
-                uploadCallback: uploadImageCallBack,
-                alt: { present: true, mandatory: true},
-              },
-            }}
-            placeholder="내용을 작성해주세요."
-            // 한국어 설정
-            localization={{
-              locale: "ko",
-            }}
-            // 초기값 설정
-            editorState={editorState}
-            // 에디터의 값이 변경될 때마다 onEditorStateChange 호출
-            onEditorStateChange={onEditorStateChange}
-          />
+          <div onDrop={handleEditorDrop}>
+            <Editor
+              // 툴바 설정
+              toolbar={{
+                //options: ['image'],
+                // inDropdown: 해당 항목과 관련된 항목을 드롭다운으로 나타낼것인지
+                list: { inDropdown: true },
+                textAlign: { inDropdown: true },
+                link: { inDropdown: true },
+                history: { inDropdown: false },
+                image: {
+                  uploadCallback: uploadImageCallBack,
+                  //alt: { present: true, mandatory: true},
+                },
+              }}
+              placeholder="내용을 작성해주세요."
+              // 한국어 설정
+              localization={{
+                locale: "ko",
+              }}
+              // 초기값 설정
+              editorState={editorState}
+              // 에디터의 값이 변경될 때마다 onEditorStateChange 호출
+              onEditorStateChange={onEditorStateChange}
+            />
+          </div>
       </div>
        <div className="notice__buttonWrap">
         <button className="notice__button" onClick={onClickHandler}>
