7613fd3892b7daafc172822c0c00d3f813f8f8a8
WM-IL000542 <wm-il000542@WM-IL000542ui-MacBookPro.local>
2023-03-03 15:08:01 +0900

    toolbar version2

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,11 @@ import createImagePlugin from '@draft-js-plugins/image';
 
 function App(this: any) {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
-  const [dom, setDom] = useState(null);
-
-  const parser = new DOMParser();
 
   const onEditorStateChange = (editorState: EditorState) => {
     // editorState에 값 설정
     setEditorState(editorState);
   };
-
-  const imagePlugin = createImagePlugin();
 
   const editorToHtml = draftToHtml(
     convertToRaw(editorState.getCurrentContent())
@@ -36,15 +31,7 @@ function App(this: any) {
   const onClickHandler = (e: any) => {
     e.preventDefault();
     try {
-      // editorToHtml 
       console.log("editorToHtml: ", editorToHtml)
-
-      // make a new parser
-      // const parser = new DOMParser();
-
-      // const rootElement = document.getElementById("root");
-      // const element = React.createElement("h1", {children : editorToHtml});
-      // ReactDOM.render(editorToHtml, rootElement);
     } catch (e) {
       console.log(e);
     }
@@ -55,13 +42,6 @@ function App(this: any) {
       <div className="notice__wrap">
           <h2 className="notice__title">내용</h2>
           <Editor
-            // 에디터와 툴바 모두에 적용되는 클래스
-            wrapperClassName="wrapper-class"
-            // 에디터 주변에 적용된 클래스
-            editorClassName="editor"
-            // 툴바 주위에 적용된 클래스
-            toolbarClassName="toolbar"
-
             // 툴바 설정
             toolbar={{
               // inDropdown: 해당 항목과 관련된 항목을 드롭다운으로 나타낼것인지
@@ -69,7 +49,6 @@ function App(this: any) {
               textAlign: { inDropdown: true },
               link: { inDropdown: true },
               history: { inDropdown: false },
-              // image:{uploadCallback:}
               image: {
                 uploadCallback: uploadImageCallBack,
                 alt: { present: true, mandatory: true},
