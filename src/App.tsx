496fe84f43d36c857934923c76a9cfe20590db43
wemade-wayne <wayne@wemade.com>
2023-03-07 17:16:25 +0900

    03071715

--- src/App.tsx
+++ src/App.tsx
@@ -118,7 +118,7 @@ function App(this : any) {
 
       setEditorState(nextEditorState);
     }, [editorState]);
-  
+    //
     const Media = () => {
         let media: JSX.Element[] = [];
         fileObjects
